45282d7d24333af65143b6edb0b094b251d84837
Breno Morais <brenomorais88@gmail.com>
2023-09-08 10:59:01 -0300

    Initial Commit

---
+++ SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Breno Morais on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
